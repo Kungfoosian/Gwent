65133e1f1717ae1fc6651290bccb3ccc7a0a8c2f
Anthony Vo <anthonyvovo123@gmail.com>
2019-06-29 00:44:46 -0700

    Add existing file 6/28/19

--- cards.js
+++ cards.js
@@ -1,36 +1,29 @@
-'use strict';
+"use strict";
 
-const FS = require('fs');
-const uuid = require('uuid');
-const JSON_PATH = 'D:/Computer Programming Files/Javascript/ExpressJS/Gwent/gwent.json';
-
-
-//create('earth elemental', 'mo', 'si', 6);
-//create('botchling', 'mo', 'me', 4);
-//create('cockatrice', 'mo', 'ra', 6);
-
-//deleteCard('cockatrice');
-
+const FS = require("fs");
+const uuid = require("uuid");
+const JSON_PATH =
+  "D:/Computer Programming Files/Javascript/ExpressJS/Gwent/gwent.json";
 
 /**Initializing cards in the database
-     * @param {String} name    Name of the card
-     * @param {String} faction 2-letter abbreviated representing card's faction
-     * @param {String} range   2-letter abbreviated representing card's range of attack
-     * @param {Number} strenth Card's attack strength
-     * @param {String} special [Card's special ability]
-     */
+ * @param {String} name    Name of the card
+ * @param {String} faction 2-letter abbreviated representing card's faction
+ * @param {String} range   2-letter abbreviated representing card's range of attack
+ * @param {Number} strenth Card's attack strength
+ * @param {String} special [Card's special ability]
+ */
 
 function create(card) {
-    let newCard = {
-        'id': uuid.v4(),
-        'name': card.name.toLowerCase(),
-        'faction': getFaction(card.faction),
-        'range': getRange(card.range),
-        'strength': card.strength,
-        'special': (card.special) ? card.special : 'n/a'
-    }
-    updateCard(undefined, newCard);
-};
+  let newCard = {
+    id: uuid.v4(),
+    name: card.name.toLowerCase(),
+    faction: card.faction,
+    range: card.range,
+    strength: card.strength,
+    special: card.special ? card.special : "n/a"
+  };
+  updateCard(undefined, newCard);
+}
 
 /**
  * Reads the JSON file and returns an array of all the cards
@@ -38,85 +31,39 @@ function create(card) {
  */
 
 function getAll() {
-    let rawData = FS.readFileSync(JSON_PATH);
-    let data = JSON.parse(rawData);
-    return data;
-};
-
-/**
- * Get faction of card
- * @param  {String} value 2-letter abbreviation, represents faction of card
- * @return {String}       Fully written out faction            
- */
-
-function getFaction(value) {
-    switch (value.toLowerCase()) {
-        case 'nr':
-            return 'northern realms';
-        case 'ni':
-            return 'nilfgaardian';
-        case 'mo':
-            return 'monsters';
-        case 'sk':
-            return 'skellige';
-        default:
-            return 'n/a';
-    };
-};
-
-/**
- * Get attack range of card
- * @param  {String} value 2-letter abbreviation, represents attack range of card
- * @return {String}       Fully written out card's attack range 
- */
-
-function getRange(value) {
-    switch (value.toLowerCase()) {
-        case 'me':
-            return 'melee';
-        case 'ra':
-            return 'ranged';
-        case 'si':
-            return 'siege';
-        default:
-            return 'n/a';
-    };
-};
-
-
+  let rawData = FS.readFileSync(JSON_PATH);
+  let data = JSON.parse(rawData);
+  return data;
+}
 
 /**
  * Update the Javscript object before calling the function to write it to JSON file
  * @param {String} cardId    Unique ID of card
- * @param {Object} cardInfo  Information of card to be added to the JSON file 
+ * @param {Object} cardInfo  Information of card to be added to the JSON file
  */
 
-function updateCard(cardId, cardInfo) {
-    let arrayOfObjects = getAll(JSON_PATH);
-    let found = arrayOfObjects.find((element) => {
-        if(typeof(cardInfo) === 'undefined'){
-            if(element['id'] === cardId){
-                return element;
-            }
-        } else if(typeof(cardId) === 'undefined') {
-            if (element['name'] === cardInfo['name']  ) {
-                return element;
-            }
-        }
-    });
+function updateCard(cardId = undefined, cardInfo) {
+  let arrayOfObjects = getAll(JSON_PATH);
+  let found = arrayOfObjects.find(element => {
+    if (element["id"] === cardId) {
+      return element;
+    } else if (element["name"] === cardInfo["name"]) {
+      return element;
+    }
+  });
 
-    if (found) {
-        console.log(`Card '${found['name']}' found. Updating...`);
-        let oldId = found['id'];
-        Object.assign(found, cardInfo);
-        found['id'] = oldId;
-    }
-    else {
-        console.log (`Adding card '${cardInfo['name']}'...`);
-        arrayOfObjects.push(cardInfo);
-    }
+  if (found) {
+    console.log(`Card '${found["name"]}' found. Updating...`);
+    let oldId = found["id"];
+    Object.assign(found, cardInfo);
+    found["id"] = oldId;
     writeCardData(arrayOfObjects);
-};
+  } else {
+    console.log(`Adding card '${cardInfo["name"]}'...`);
+    arrayOfObjects.push(cardInfo);
+    writeCardData(arrayOfObjects);
+  }
+}
 
 /**
  * Edit the JSON file
@@ -125,80 +72,75 @@ function updateCard(cardId, cardInfo) {
  */
 
 function writeCardData(cardInfo) {
-    FS.writeFileSync(JSON_PATH, JSON.stringify(cardInfo, null, 5));
-    console.log(`Database was updated successfully \n`);
-};
-
-
+  FS.writeFileSync(JSON_PATH, JSON.stringify(cardInfo, null, 5));
+  console.log(`Database was updated successfully \n`);
+}
 
 /**
  * Read the JSON file but returns only the card requested
  * @param   {String} idCardToGet  id of the card to get
- * @returns                     String value of the card if found, error if not 
+ * @returns                     String value of the card if found, error if not
  */
 
 function getOne(idOfCardToGet) {
-    let arrayOfObjects = getAll(JSON_PATH);
-    let cardFound = arrayOfObjects.filter(card => card['id'] === idOfCardToGet);
+  let arrayOfObjects = getAll(JSON_PATH);
+  let cardFound = arrayOfObjects.filter(card => card["id"] === idOfCardToGet);
 
-    if (cardFound.length > 0) {
-        return cardFound;
-    }
-    else {
-        return 'Card not found';
-    }
-};
+  if (cardFound.length > 0) {
+    return cardFound;
+  } else {
+    return "Card not found";
+  }
+}
 
 /**
  * Delete card from Jsvascript object and then call the function to update the JSON database
  * @param {String} IdCardToRemove Unique ID of card to remove
- * 
+ *
  */
 
 function deleteCard(IdCardToRemove) {
-    let arrayOfObjects = getAll(JSON_PATH);
-    let found = arrayOfObjects.find(function (element) {
-        if (element['id'] === IdCardToRemove) {
-            return element;
-        }
-    });
-
-    if (found) {
-        let index = getIndex(arrayOfObjects, found['id']);
-        console.log(`Card '${found['name']}' found. Removing...`);
-        arrayOfObjects.splice(index, 1);
-        writeCardData(arrayOfObjects);
+  let arrayOfObjects = getAll(JSON_PATH);
+  let found = arrayOfObjects.find(function(element) {
+    if (element["id"] === IdCardToRemove) {
+      return element;
     }
-    else {
-        console.log(`Card '${IdCardToRemove}' does not exist.`);
-    }
+  });
 
-};
+  if (found) {
+    let index = getIndex(arrayOfObjects, found["id"]);
+    console.log(`Card '${found["name"]}' found. Removing...`);
+    arrayOfObjects.splice(index, 1);
+    writeCardData(arrayOfObjects);
+  } else {
+    console.log(`Card '${IdCardToRemove}' does not exist.`);
+  }
+}
 
 /**
  * get position of an object in an array of objects
  * @param   {Array of Objects} arrayOfObjects Array of Objects
  * @param   {String}           id             Unique ID of object to look for in the array
- * @returns                                   Index of the object element  
+ * @returns                                   Index of the object element
  */
 
 function getIndex(arrayOfObjects, id) {
-    for (let i = 0; i < arrayOfObjects.length; i++) {
-        if (arrayOfObjects[i]['id'] === id) {
-            return i;
-        }
-        else if (i === arrayOfObjects.length && arrayOfObjects[i]['name'] !== elementValue) {
-            return `Card '${elementValue}' not found`;
-        }
+  for (let i = 0; i < arrayOfObjects.length; i++) {
+    if (arrayOfObjects[i]["id"] === id) {
+      return i;
+    } else if (
+      i === arrayOfObjects.length &&
+      arrayOfObjects[i]["name"] !== elementValue
+    ) {
+      return `Card '${elementValue}' not found`;
     }
-};
-
-
+  }
+}
 
 module.exports = {
-    create,
-    getAll,
-    getOne,
-    updateCard,
-    deleteCard
+  create,
+  getAll,
+  getOne,
+  updateCard,
+  deleteCard
 };
--- index.js
+++ index.js
@@ -1,87 +1,48 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 5000;
-const cards = require('./cards.js');
-const bodyParser = require('body-parser');
+const cards = require("./cards.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Middleware
-// app.use(bodyParser());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
-})
-
-app.get('/cards', (req, res) => {
-    res.json(cards.getAll());
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
 });
 
-app.get('/cards/:id', (req, res) => {
-    res.json(cards.getOne(req.params.id));
+app.get("/cards", (req, res) => {
+  res.json(cards.getAll());
+});
+
+app.get("/cards/:id", (req, res) => {
+  res.json(cards.getOne(req.params.id));
 });
 
 //Creating cards
-app.post('/cards', (req, res) => {
-    let errors = preCheck(req.body);
+app.post("/cards", (req, res) => {
+  //let errors = preCheck(req.body);
 
-    //Results
-    if (errors.length > 0) {
-        res.status(400).json({ 'msg': 'Please fix the following error(s):', 'errors': errors });
-    } else {
-        res.json(cards.create(req.body));
-    }
+  //Results
+  res.json(cards.create(req.body));
 });
 
 //Update card
-app.put('/cards/:id', (req, res) => {
-    let errors = preCheck(req.body);
-
-    if (errors.length > 0) {
-        res.status(400).json({ 'msg': 'Please fix the following error(s):', 'errors': errors });
-    }
-    else {
-        res.json(cards.updateCard(req.params.id, req.body));
-    }
-
+app.put("/cards/:id", (req, res) => {
+  res.json(cards.updateCard(req.params.id, req.body));
 });
 
 //Delete card
-app.delete('/cards/:id', (req, res) => {
-    res.json(cards.deleteCard(req.params.id));
+app.delete("/cards/:id", (req, res) => {
+  res.json(cards.deleteCard(req.params.id));
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
-
-
-
-//Precheck and input error handling
-const preCheck = (object) => {
-    let errors = [];
-    Object.keys(object).forEach((key) => {
-        if (key === 'name') {
-            if (!object['name'] || !isNaN(object['name'])) {
-                errors.push('Name must be a string, no numbers');
-            }
-        } else if (key === 'faction') {
-            if (!object['faction'] || !isNaN(object['faction']) || object['faction'].length !== 2) {
-                errors.push('Faction must be a two character string, no numbers');
-            }
-        } else if (key === 'range') {
-            if (!object['range'] || !isNaN(object['range']) || object['range'].length !== 2) {
-                errors.push('Range must be a two character string, no numbers');
-            }
-        } else if (key === 'strength') {
-            if (!object['strength'] || isNaN(object['strength']) || object['strength'] < 1 || object['strength'] > 10) {
-                errors.push('Strength must be a number from 1 to 10');
-            }
-        }
-    });
-    return errors;
-}
